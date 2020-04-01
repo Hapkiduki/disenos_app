ace1195e13b6d7e7e10a959ea815b0e2cf57d4f3
Andres Corrales <Andres.Corrales@o-i.com>
2020-04-01 00:26:52 -0500

    Se trabaja con animaciones mediante animationBuilder

--- lib/src/pages/headers_page.dart
+++ lib/src/pages/headers_page.dart
@@ -1,11 +1,13 @@
-import 'package:disenos_app/src/widgets/headers.dart';
+//import 'package:disenos_app/src/widgets/headers.dart';
 import 'package:flutter/material.dart';
+import 'package:disenos_app/src/retos/cuadrado_animado_page.dart';
+//import 'package:disenos_app/src/pages/animaciones_page.dart';
 
 class HeadersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HeaderWave(),
+      body: CuadradoAnimadoPage(),
     );
   }
 }
